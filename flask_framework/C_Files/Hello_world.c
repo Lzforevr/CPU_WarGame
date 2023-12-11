34f28e775276a825526760e3af8167e06b96e113
Lzforevr <139556927+Lzforevr@users.noreply.github.com>
2023-12-11 10:22:30 +0800

    Delete flask_framework directory

--- flask_framework/C_Files/Hello_world.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-
-    printf("Hello World\n");
-    fflush(stdout);
-    return 0;
-}
--- flask_framework/exts.py
+++
@@ -1,5 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_mail import Mail
-
-db = SQLAlchemy()
-mail = Mail()
